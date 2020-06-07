59a07590b737050a0e4d27773b412b92db16d5b4
Tokae100@uni-duesseldorf.deC:/Users/tobia/anaconda3/envs/testenv/python.exe <Tokae100@uni-duesseldorf.deC:/Users/tobia/anaconda3/envs/testenv/python.exe>
2020-06-07 18:27:15 +0200

    in test.py den zeitgraph auf 3 zeiten aktualisiert somit ist die Zeitmessung jetzt möglich und muss nurnoch im source code implementiert werden

--- tests.py
+++ tests.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 import time
 import matplotlib.pyplot as plt
+import itertools
 print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
 config = tf.ConfigProto()
 config.gpu_options.allow_growth = True
@@ -27,28 +28,50 @@ model.add(Dense(1,activation="sigmoid"))
 
 model.compile(optimizer="rmsprop",loss="binary_crossentropy",metrics=["accuracy"])
 
-timeline = []
+timeline1 = []
+timeline2 = []
+timeline3 = []
 
 print(X_train.shape)
-y_train_car = y_train == 1 
+y_train_car = y_train == 1
+sum = 0; 
 for i in range(5):
+    tic = time.perf_counter()
+    for i in range(100000000):
+        sum = sum + i
+    toc = time.perf_counter()
+    toc = float(toc - tic)
+    timeline1.append(toc)
     tic = time.perf_counter()
     model.fit(X_train,y_train_car,batch_size=256,epochs=1,shuffle=True)
     toc = time.perf_counter()
     toc = float(toc - tic)
-    timeline.append(toc)
+    timeline2.append(toc)
+    tic = time.perf_counter()
+    for i in range(50000000):
+        sum = sum - i
+    toc = time.perf_counter()
+    toc = float(toc - tic)
+    timeline3.append(toc)
+    
+
 
 
 bar_list1 = []
 bar_list2 = []
+bar_list3 = []
 progress = 0
-for i in range(len(timeline)):
-    timeline[i] = round(timeline[i],3)*1000
-    if i%2 == 0:
-        bar_list1.append((progress,timeline[i]))
-    else:
-        bar_list2.append((progress,timeline[i]))
-    progress = progress + timeline[i]
+for (entry1,entry2,entry3) in itertools.zip_longest(timeline1,timeline2,timeline3):
+
+    if(entry1!=None):
+        bar_list1.append((progress,round(entry1,3)*1000))
+        progress = progress + round(entry1,3)*1000
+    if(entry2!=None):
+        bar_list2.append((progress,round(entry2,3)*1000))
+        progress = progress + round(entry2,3)*1000
+    if(entry3!=None):
+        bar_list3.append((progress,round(entry3,3)*1000))
+        progress = progress + round(entry3,3)*1000
 
 print(bar_list1)
 
@@ -56,17 +79,18 @@ print(bar_list1)
 
 fig, gra = plt.subplots()
 
-gra.set_ylim(0,40)
-gra.set_xlim(0,10000)
+gra.set_ylim(0,55)
+gra.set_xlim(0,100000)
 
 gra.set_xlabel("seconds since start")
 gra.set_ylabel("durchführung")
 
-gra.set_yticks([15,30])
-gra.set_yticklabels(["selfplay","netzwerktraining"])
+gra.set_yticks([15,30,45])
+gra.set_yticklabels(["selfplay","netzwerktraining","arena"])
 
-gra.broken_barh(bar_list1,(15,9),facecolors='tab:blue')
-gra.broken_barh(bar_list2,(30,9),facecolors='tab:red')
+gra.broken_barh(bar_list1,(11,9),facecolors='tab:blue')
+gra.broken_barh(bar_list2,(26,9),facecolors='tab:red')
+gra.broken_barh(bar_list3,(40,9),facecolors='tab:green')
 plt.show()
 
 
